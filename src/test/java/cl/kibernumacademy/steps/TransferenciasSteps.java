f11255a14c1157c2593308cce899627d6ddf725f
kenkairon <ingeinfo2006@gmail.com>
2025-08-02 20:52:47 -0400

    prueba

--- src/test/java/cl/kibernumacademy/steps/TransferenciasSteps.java
+++ src/test/java/cl/kibernumacademy/steps/TransferenciasSteps.java
@@ -11,12 +11,12 @@ public class TransferenciasSteps {
 
     @Given("the user is logged in")
     public void el_usuario_ha_iniciado_sesion() {
-        System.out.println("➡️ Usuario ha iniciado sesión correctamente.");
+        System.out.println("Usuario ha iniciado sesión correctamente.");
     }
 
     @When("the user transfers {int} to account {int}")
     public void the_user_transfers_to_account(Integer monto, Integer cuenta) {
-        System.out.println("💸 Transfiriendo " + monto + " a la cuenta " + cuenta);
+        System.out.println("Transfiriendo " + monto + " a la cuenta " + cuenta);
 
         if (monto <= 0) {
             mensaje = "Monto inválido";
@@ -29,7 +29,7 @@ public class TransferenciasSteps {
 
     @Then("the user should see the message {string}")
     public void the_user_should_see_the_message(String esperado) {
-        System.out.println("✅ Esperado: " + esperado);
+        System.out.println("Esperado: " + esperado);
         System.out.println("🔍 Obtenido: " + mensaje);
         Assertions.assertEquals(esperado, mensaje);
     }
